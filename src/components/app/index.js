c3f92d08ed1648f31d8a0311ba72037705a2991e
Ryan Temple <temple.design.me@gmail.com>
2018-08-04 10:57:19 -0400

    initial commit

---
+++ src/components/app/index.js
@@ -0,0 +1,12 @@
+//NPM
+import React, { Component, Fragment } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>App</Fragment>
+    )
+  }
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,16 @@
+//NPM
+import React from 'react'
+import ReactDOM from 'react-dom'
+//import registerServiceWorker from './registerServiceWorker'
+
+//Components
+import App from './components/app'
+
+//CSS
+import './styles/index.css'
+
+ReactDOM.render(
+  <App/>, 
+  document.getElementById('root')
+)
+//registerServiceWorker();
